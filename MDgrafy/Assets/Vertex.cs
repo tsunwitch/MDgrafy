e89c36c670443c8290051e9f53b5f4b0c6a483b6
Patryk Sablik <sablikpatryk10@gmail.com>
2022-04-21 17:05:03 +0200

    Poprawa wizualna vertexow

--- MDgrafy/Assets/Vertex.cs
+++ MDgrafy/Assets/Vertex.cs
@@ -25,20 +25,29 @@ namespace MDgrafy.Assets
         {
             X = rnd.Next((int)cnvs.Width);
             Y = rnd.Next((int)cnvs.Height);
+
+            // Poprawa vertexow na krawedziach
+            if (X >= (int)cnvs.Width - 35) X -= 35;
+            if (X <= 35) X += 35;
+
+            if (Y >= (int)cnvs.Height - 25) Y -= 35;
+            if (Y <= 35) Y += 35;
+
             Index = indx + 1;
 
             Ellipse ellipse = new Ellipse()
             {
                 Width = 25,
                 Height = 25,
-                Fill = new BrushConverter().ConvertFromString("Aqua") as Brush
+                Fill = new BrushConverter().ConvertFromString("#4A148C") as Brush
             };
 
             Label label = new Label()
             {
                 FontSize = 13,
                 Content = Convert.ToString(index + 1),
-                FontWeight = FontWeights.Bold
+                FontWeight = FontWeights.Bold,
+                Foreground = new BrushConverter().ConvertFromString("#dec9e9") as Brush
             };
 
             cnvs.Children.Add(ellipse);
@@ -47,16 +56,10 @@ namespace MDgrafy.Assets
             ellipse.SetValue(Canvas.LeftProperty, this.X);
             ellipse.SetValue(Canvas.TopProperty, this.Y);
 
-            if (index > 9)
-            {
-                label.SetValue(Canvas.LeftProperty, this.x - 3);
-                label.SetValue(Canvas.TopProperty, this.y - 5);
-            }
-            else
-            {
-                label.SetValue(Canvas.LeftProperty, this.x + 1);
-                label.SetValue(Canvas.TopProperty, this.y - 5);
-            }
+            // Poprawa ustawienia labela w vertexie
+            double labelOffset = (Index <= 8) ? this.x + 5 : this.x;
+            label.SetValue(Canvas.LeftProperty, labelOffset);
+            label.SetValue(Canvas.TopProperty, this.y);
         }
     }
 }
